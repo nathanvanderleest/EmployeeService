716a64ce98d367119379890fb8db2e7bc0af9f6b
Nate <nathanvanderleest1@gmail.com>
2017-09-04 01:10:31 -0500

    added EmployeeDataAccess Project & created EmployeesController.cs

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+
+        // GET api/employee
+        public IEnumerable<employee> Get()
+        {
+            using(TestDBEntities entities = new TestDBEntities())
+            {
+                return entities.employees.ToList();
+            }
+        }
+
+        // GET api/employee/5
+        public employee Get(int id)
+        {
+            using (TestDBEntities entities = new TestDBEntities())
+            {
+                return entities.employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+
+
+    }
+}
