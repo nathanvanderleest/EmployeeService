0e94f13eb6a7ea013b738318f32f70f7768fb3cd
Nate <nathanvanderleest1@gmail.com>
2017-09-04 19:43:59 -0500

    added CustomJsonFormtter class to return json or xml, but only json to the browser

--- EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -3,9 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace EmployeeService
 {
+
+    public class CustomJsonFormatter : JsonMediaTypeFormatter
+    {
+        public CustomJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+
+        public override JsonSerializer CreateJsonSerializer()
+        {
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            jsonSerializer.Formatting = Newtonsoft.Json.Formatting.Indented;
+            jsonSerializer.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            return jsonSerializer;
+        }
+
+    }
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -26,6 +53,12 @@ namespace EmployeeService
 
             // Camel case instead of Pascal case
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.Add(new CustomJsonFormatter());
+
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+
         }
     }
 }
